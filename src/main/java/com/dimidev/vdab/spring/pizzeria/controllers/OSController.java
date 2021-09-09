e9fc4baa351f46c1d4e5785d6ab01a435c573dbe
dmtwood <dimitri.gevers@gmail.com>
2021-09-09 09:33:55 +0200

    useragent as param

---
+++ src/main/java/com/dimidev/vdab/spring/pizzeria/controllers/OSController.java
@@ -0,0 +1,39 @@
+package com.dimidev.vdab.spring.pizzeria.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+
+@Controller
+@RequestMapping("/os")
+class OSController {
+
+// MEMBER VARS
+
+    private static final String[] OSs = { "Windows", "Linux", "Android", "Macintosh"};
+
+// CONSTRUCTORS
+
+
+// GETTERS ( & SETTERS IF MUTABLE)
+
+
+// METHODS
+
+    @GetMapping
+    public ModelAndView os(@RequestHeader("User-agent") String userAgent) {
+        ModelAndView osView = new ModelAndView("os");
+        Arrays.stream(OSs)
+                .filter( userAgent::contains )
+                .findFirst()
+                .ifPresent( osString -> osView.addObject("osString", osString));
+        return osView;
+    }
+
+// OVERRIDDEN METHODS
+
+}
